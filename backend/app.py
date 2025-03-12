3f1d63bfb431e8c009d028478bb46a6ff264f0d0
Jad El Asmar <42979241+1elasmarjad@users.noreply.github.com>
2025-03-12 16:36:00 -0400

    Added `proof_solver_websocket`

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,38 @@
 from argparse import ArgumentParser, Namespace
 import asyncio
-from fastapi import FastAPI, WebSocket
+import logging
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 app = FastAPI(dependencies=[])
 
-@app.get("/")
 
+@app.get("/")
 async def root():
     return {"message": "World"}
 
+
 @app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    print("WebSocket connected")
-    await websocket.accept()
-    while True:
-        await websocket.send_text(f"Hello!")
-        # sleep for 1 second
-        await asyncio.sleep(1)
+async def proof_solver_websocket(websocket: WebSocket):
+    """
+    WebSocket endpoint for proof solver, this endpoint is used to solve the proof and communicate with the client.
+    """
+    logging.info("[WebSocket] connection attempt")
+
+    try:
+        await websocket.accept()
+    except Exception as e:
+        logging.error(f"[WebSocket] Error: {e}")
+        raise e
+
+    logging.info("[WebSocket] connection established")
+
+    try:
+        while True:
+            await websocket.send("Hello!")
+            # sleep for 1 second
+            await asyncio.sleep(1)
+    except WebSocketDisconnect:
+        logging.info("[WebSocket] disconnected")
+    except Exception as e:
+        logging.error(f"[WebSocket] Error: {e}")
+        raise e
